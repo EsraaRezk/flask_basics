f58bf366dbc871a952c811a643eddd1b8afcf91b
esraarezk <esraa.hassan2@msa.edu.eg>
2021-09-13 22:10:38 +0400

    new project

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-from flask import Flask, request
+from datetime import datetime
+
+from flask import Flask, request, session, g
 from flask import render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 app.config.update(
-    SECRET_KET='topsecret',
-    SQLAlchemy_DATABASE_URI='<database>://<user_id>:<password>@server>/<database_name>',
+    SECRET_KEY='topsecret',
+    SQLALCHEMY_DATABASE_URI='postgresql://postgres:kony2012@localhost/catalog_db',
     SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
+db = SQLAlchemy(app)
+
+@app.before_request
+def func():
+    g.string = '<br> This code runs before any request'
 
 @app.route('/')
 @app.route('/index')
 def hello_flask():
-    return 'Hello oooks'
+    return 'Hello oooks <br>' + g.string
 
 @app.route('/new/')
 def query_string(greeting = 'hello'):
@@ -73,6 +79,53 @@ def macros():
     return render_template('using_macros.html', movies=movie_list)
 
 
+@app.route('/session')
+def session_data():
+    if 'name' not in session:
+        session['name'] = 'harry'
+    return render_template('session.html', session=session, name=session['name'])
+
+
+class Publication(db.Model):
+    __tablename__ = 'publication'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return 'Name is {}'.format(self.name)
+
+
+class Book(db.Model):
+    __tablename__ = 'book'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(500), nullable=False, index=True)
+    author = db.Column(db.String(350))
+    avg_rating = db.Column(db.Float)
+    format = db.Column(db.String(50))
+    image = db.Column(db.String(100), unique=True)
+    num_pages = db.Column(db.Integer)
+    pub_date = db.Column(db.DateTime, default=datetime.utcnow())
+
+    pub_id = db.Column(db.Integer, db.ForeignKey('publication.id'))
+
+    def __init__(self, title, author, avg_rating, format, image, num_pages, pub_id):
+        self.title = title
+        self.author = author
+        self.avg_rating = avg_rating
+        self.format = format
+        self.image = image
+        self.num_pages = num_pages
+        self.pub_id = pub_id
+
+    def __repr__(self):
+        return '{} by {}'.format(self.title, self.author)
 # Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    db.create_all()
+    app.run(debug=True)
